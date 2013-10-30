c664ce791c0734200b1899cb56b289980f7c1129
anovitskiy <anovitskiy+github@gmail.com>
2013-10-30 10:40:45 -0700

    per Drews recommendation, fix handling of null (undef) parameter passed into PS command

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
 define download_file ($url, $destination, $proxyAddress=undef) {
   $filename = regsubst($url, '^http.*\/([0-9a-zA-Z_.-]+.exe)$', '\1')
   
-  $psCommand = "\$webclient = New-Object System.Net.WebClient;\$proxyAddress = ${proxyAddress};if (\$proxyAddress -ne \$null) {if (!\$proxyAddress.StartsWith('http://')) { \$proxyAddress = 'http://' + \$proxyAddress }\$proxy = new-object System.Net.WebProxy;\$proxy.Address = \$proxyAddress;\$webclient.proxy = \$proxy;}try {\$webclient.DownloadFile('${url}', '${destination}\\${filename}')} catch [Exception] {write-host \$_.Exception.GetType().FullName; write-host \$_.Exception.Message; write-host \$_.Exception.InnerException.Message; throw \$_.Exception;}"
+  $psCommand = "\$webclient = New-Object System.Net.WebClient;\$proxyAddress = '${proxyAddress}';if (\$proxyAddress -ne '') {if (!\$proxyAddress.StartsWith('http://')) { \$proxyAddress = 'http://' + \$proxyAddress }\$proxy = new-object System.Net.WebProxy;\$proxy.Address = \$proxyAddress;\$webclient.proxy = \$proxy;}try {\$webclient.DownloadFile('${url}', '${destination}\\${filename}')} catch [Exception] {write-host \$_.Exception.GetType().FullName; write-host \$_.Exception.Message; write-host \$_.Exception.InnerException.Message; throw \$_.Exception;}"
   validate_re($url, '.+')
   validate_re($destination, '.+')
   validate_re($filename, '.+')
