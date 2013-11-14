90dbf51df6d33591c8288bbe762b40cfea1d545f
stack72 <public@paulstack.co.uk>
2013-11-14 13:16:53 +0000

    Fixing the file paths

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,13 +6,13 @@ define download_file ($url, $destination, $proxyAddress=undef) {
   validate_re($filename, '.+')
 
   file { "download-${filename}.ps1" :
-    path    => "c:\temp\download.ps1",
+    path    => "c:\\temp\\download.ps1",
     ensure  => present,
     content => template('download_file/download.ps1.erb'),
   }
 
   exec { "download-${filename}" :
-    command   => 'c:\temp\download.ps1',
+    command   => "c:\\temp\\download.ps1",
     provider  => powershell,
     onlyif    => "if(Test-Path -Path '${destination}\\${filename}') { exit 1 } else { exit 0 }",
     logoutput => true,
