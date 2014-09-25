f2a8adb827ff39e74b95107d0e10356aa83751ce
liamjbennett <lbennett@opentable.com>
2014-09-25 21:39:40 +0100

    fixing windows config in spec_helper_acceptance.rb

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -4,6 +4,11 @@ require 'beaker-rspec/helpers/serverspec'
 
 hosts.each do |host|
   
+	if host['platform'] =~ /windows/
+	   include Serverspec::Helper::Windows
+		 include Serverspec::Helper::WinRM
+	end
+	
   
 	version = ENV['PUPPET_GEM_VERSION']
 	install_puppet(:version => version)
@@ -22,6 +27,12 @@ Spec.configure do |c|
 			c.host = host
       
 			
+      if host['platform'] =~ /windows/
+				endpoint = "http://127.0.0.1:5985/wsman"
+				c.winrm = ::WinRM::WinRMWebService.new(endpoint, :ssl, :user => 'vagrant', :pass => 'vagrant', :basic_auth_only => true)
+				c.winrm.set_timeout 300
+		  end
+      
 
 			path = (File.expand_path(File.dirname(__FILE__)+'/../')).split('/')
 			name = path[path.length-1].split('-')[1]
