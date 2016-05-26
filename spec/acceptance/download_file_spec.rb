c2847e37d887ac9683e1c44cf2449bd06b66b3ae
Tim Meusel <tim@bastelfreak.de>
2016-05-26 20:14:00 +0200

    rubocop: fix RSpec/ExampleWording

--- spec/acceptance/download_file_spec.rb
+++ spec/acceptance/download_file_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper_acceptance'
 
 describe 'download_file' do
   context 'dowload_file should download dotnet 4' do
-    it 'should download dotnet 4' do
+    it 'downloads dotnet 4' do
       pp = <<-PP
         download_file { "Download dotnet 4.0" :
           url                   => 'http://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe',
